7ca329901be3d100bedfe8ae35b2447277d99248
i3oi3ka <i3oi3ka@ukr.net>
2025-05-19 01:10:50 +0300

    Refactor movie data transformation to use default image for missing posters

--- src/utils/transformMovie.js
+++ src/utils/transformMovie.js
@@ -1,4 +1,6 @@
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/";
+const defaultImg =
+  "https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg";
 
 const transformMoviesData = (data) => {
   return data.map(({ id, title }) => ({
@@ -12,7 +14,7 @@ const transformMovieData = (data) => {
     title: data.title,
     vote: Math.round(Number(data.vote_average) * 10),
     release_year: data.release_date.slice(0, 4),
-    poster: IMAGE_BASE_URL + data.poster_path,
+    poster: data.poster ? IMAGE_BASE_URL + data.poster_path : defaultImg,
     overview: data.overview,
     genres: data.genres.map(({ name }) => name),
   };
@@ -23,9 +25,7 @@ const transformMovieCast = (data) => {
     id,
     name,
     character,
-    profile_path:
-      IMAGE_BASE_URL +
-      (profile_path ? profile_path : "/1E5baAaEse26fej7uHcjOgEE2t2.jpg"),
+    profile_path: profile_path ? IMAGE_BASE_URL + profile_path : defaultImg,
   }));
 };
 
